dc65a26b10b2351ebc73cfe982f41cb8a6ae2011
NFS03 <jarbull03@gmail.com>
2022-08-02 18:39:45 +0700

    NFS-FileSharingBot Deploy | Tue Aug  2 18:39:45 WIB 2022

--- backups/backup.sql
+++ backups/backup.sql
@@ -16,6 +16,40 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: broadcast; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.broadcast (
+    id numeric NOT NULL,
+    user_name text
+);
+
+
+--
+-- Data for Name: broadcast; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.broadcast (id, user_name) FROM stdin;
+1441016790	@KurangKopiGes
+992305042	@mobxprjkt
+582339444	@Yogyess0
+1004401454	@N_mans
+\.
+
+
+--
+-- Name: broadcast broadcast_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.broadcast
+    ADD CONSTRAINT broadcast_pkey PRIMARY KEY (id);
+
+
 --
 -- PostgreSQL database dump complete
 --
